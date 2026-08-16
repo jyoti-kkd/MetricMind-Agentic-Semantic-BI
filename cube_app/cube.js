6d13e9f7c6f1a90ad25400edb2288bf93be9925a
Jyoti Kankanawadi <jyotikankanawadi162@gmail.com>
2026-08-16 23:08:55 +0530

    Add Cube.js application setup

---
+++ cube_app/cube.js
@@ -0,0 +1,13 @@
+const Cube = require("@cubejs-backend/server");
+
+module.exports = {
+  driverFactory: () => ({
+    type: "snowflake",
+    account: process.env.CUBEJS_DB_ACCOUNT,
+    username: process.env.CUBEJS_DB_USER,
+    password: process.env.CUBEJS_DB_PASS,
+    database: process.env.CUBEJS_DB_NAME,
+    schema: process.env.CUBEJS_DB_SCHEMA,
+    warehouse: process.env.CUBEJS_DB_WAREHOUSE
+  })
+};
